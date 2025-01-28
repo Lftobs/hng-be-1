60a80db46f58eb3779dedfa2ca3aa29d82855345
lftobs <tobbytobs1@gmail.com>
2025-01-28 22:38:28 +0100

    s1 task

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status 
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-import requests
-from dotenv import load_dotenv
-import os
-import requests
+import datetime
 
 app = FastAPI()
 app.add_middleware(
@@ -14,44 +11,24 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-load_dotenv()
 
-def get_real_ip(request: Request):
-    headers = request.headers
-    if 'x-forwarded-for' in headers:
-        return headers['x-forwarded-for'].split(',')[0].strip()
-    elif 'x-real-ip' in headers:
-        return headers['x-real-ip']
-    else:
-        return request.client.host
+# def get_real_ip(request: Request):
+#     headers = request.headers
+#     if 'x-forwarded-for' in headers:
+#         return headers['x-forwarded-for'].split(',')[0].strip()
+#     elif 'x-real-ip' in headers:
+#         return headers['x-real-ip']
+#     else:
+#         return request.client.host
 
-@app.get("/api/hello")
-async def greeting(request: Request, visitor_name: str):
-    ip_add = get_real_ip(request)
-    name = visitor_name.replace('"', '')
-    url  = f'https://api.weatherapi.com/v1/current.json?q={ip_add}&key={os.getenv("KEY")}'
-    res = requests.get(url)
-    data = res.json()
-    location = data["location"].get('region', 'Delhi')
-    text = f'Hello, {name}!, the temperature is {data["current"]["temp_c"]} degrees Celsius in {location}'
-    
+@app.get("/")
+async def main():
+    email ='tobbytobs1@gmail.com'
+    date = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    github_url = 'https://gi.com'
     return JSONResponse(content={
-        "client_ip": ip_add,
-        "location": location,
-        "greeting": text,
-    })
+        "email": email,
+        "current_datetime": date,
+        "github_url": github_url,
+    }, status_code=status.HTTP_200_OK)
 
-@app.get('/api/products')
-async def get_products(request: Request):
-    url = f'https://api.timbu.cloud/products?{request.query_params}'
-    print(url)
-    res = requests.get(url)
-    data = res.json()
-    return JSONResponse(content=data)
-
-@app.get('/api/products/{product_id}')
-async def get_products(product_id: str, request: Request):
-    url = f'https://api.timbu.cloud/products/{product_id}?{request.query_params}'
-    res = requests.get(url)
-    data = res.json()
-    return JSONResponse(content=data)
